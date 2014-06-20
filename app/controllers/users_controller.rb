6ab79ca8c5d13a0a7d9ca5a1541d3eacab402b7f
Martin Granados <margrangr@gmail.com>
2014-06-19 20:38:59 -0500

    Antes de pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 include SessionsHelper
+before_action :signed_in_user, only: [:index, :edit, :update]
+before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -20,9 +22,37 @@ include SessionsHelper
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
